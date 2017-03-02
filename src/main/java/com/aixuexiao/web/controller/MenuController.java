ef3528b15a924dcec25de355385199263e2c747c
plseal <ttml0122@gmail.com>
2017-03-02 13:01:52 +0900

    import-ResponseBody

--- src/main/java/com/aixuexiao/web/controller/MenuController.java
+++ src/main/java/com/aixuexiao/web/controller/MenuController.java
@@ -10,6 +10,7 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aixuexiao.model.Classes;
